40df7858bff0af6842bacc611489f927c7bb2098
Tai Lee <real.human@mrmachine.net>
2019-02-17 20:34:46 +1100

    Just do `restic prune` instead of `forget --prune`. Forget is already executed after every backup.

--- bin/prune.sh
+++ bin/prune.sh
@@ -5,12 +5,7 @@ set -e
 setup.sh
 
 echo "Pruning old snapshots"
-while ! restic forget \
-		--prune \
-		--keep-hourly="${RESTIC_KEEP_HOURLY:-24}" \
-		--keep-daily="${RESTIC_KEEP_DAILY:-7}" \
-		--keep-weekly="${RESTIC_KEEP_WEEKLY:-4}" \
-		--keep-monthly="${RESTIC_KEEP_MONTHLY:-12}"; do
+while ! restic prune; do
 	echo "Sleeping for 1 second before retry..."
 	sleep 1
 done
