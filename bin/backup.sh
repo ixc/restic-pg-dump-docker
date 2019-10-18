c8da69d7475092f78b1f72265f0179ecd3f67fdf
Tai Lee <real.human@mrmachine.net>
2019-10-18 15:49:05 +1100

    Forget snapshots immediately before pruning. Test repository once a week after pruning and remove unwanted cache.

--- bin/backup.sh
+++ bin/backup.sh
@@ -20,21 +20,7 @@ for i in {1..5}; do
 	# No more databases.
 	for var in PGHOST PGUSER; do
 		[[ -z "${!var}" ]] && {
-			echo "Forgetting old snapshots"
-			while ! restic forget \
-					--compact \
-					--keep-hourly="${RESTIC_KEEP_HOURLY:-24}" \
-					--keep-daily="${RESTIC_KEEP_DAILY:-7}" \
-					--keep-weekly="${RESTIC_KEEP_WEEKLY:-4}" \
-					--keep-monthly="${RESTIC_KEEP_MONTHLY:-12}"; do
-				echo "Sleeping for 10 seconds before retry..."
-				sleep 10
-			done
-
-			restic check --no-lock
-
 			echo 'Finished backup successfully'
-
 			exit 0
 		}
 	done
--- bin/prune.sh
+++ bin/prune.sh
@@ -4,7 +4,16 @@ set -e
 
 setup.sh
 
-rm -rf /tmp/restic-check-cache-*
+echo "Forgetting old snapshots"
+while ! restic forget \
+		--compact \
+		--keep-hourly="${RESTIC_KEEP_HOURLY:-24}" \
+		--keep-daily="${RESTIC_KEEP_DAILY:-7}" \
+		--keep-weekly="${RESTIC_KEEP_WEEKLY:-4}" \
+		--keep-monthly="${RESTIC_KEEP_MONTHLY:-12}"; do
+	echo "Sleeping for 10 seconds before retry..."
+	sleep 10
+done
 
 echo "Pruning old snapshots"
 while ! restic prune; do
@@ -12,6 +21,8 @@ while ! restic prune; do
 	sleep 10
 done
 
+# Test repository and remove unwanted cache.
 restic check --no-lock
+rm -rf /tmp/restic-check-cache-*
 
 echo 'Finished prune successfully'
