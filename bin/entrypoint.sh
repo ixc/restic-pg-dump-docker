9f0d89a62092977b9303a53e9c733866adbe2d93
Tai Lee <real.human@mrmachine.net>
2022-09-13 14:25:37 +1000

    Do not use the restic-pg-dump container's hostname as the default for `HOSTNAME_1`.
    When `HOSTNAME_N` is not provided, it should fallback to `PGHOST_N`, instead.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,7 +3,6 @@
 set -e
 
 # Get config for first database from environment variables with no counter.
-export HOSTNAME_1="${HOSTNAME_1:-$HOSTNAME}"
 export PGHOST_1="${PGHOST_1:-${PGHOST:-postgres}}"
 export PGPASSWORD_1="${PGPASSWORD_1:-$PGPASSWORD}"
 export PGPORT_1="${PGPORT_1:-${PGPORT:-5432}}"
